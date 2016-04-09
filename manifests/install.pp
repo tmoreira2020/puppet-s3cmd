827ae4d78d18fa282e41617fbce6fd238f883251
Thiago Leão Moreira <tmoreira2020@gmail.com>
2016-04-09 10:55:40 -0300

    Format source

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,11 @@
 class s3cmd::install {
 
-  package {"python-setuptools":
-    ensure     => "installed"
+  Exec {
+    path => '/bin',
+  }
+
+  package {'python-setuptools':
+    ensure => 'installed'
   }
 
   wget::fetch { 'fetch-s3cmd':
@@ -11,15 +15,13 @@ class s3cmd::install {
     before      => Exec['untar-s3cmd'],
   }
 
-  exec { "untar-s3cmd":
-    path       => "/bin",
-    command    => "tar -zxvf /tmp/s3cmd-${s3cmd::params::commit_sha1_abbreviation}.tar.gz -C /tmp"
+  exec { 'untar-s3cmd':
+    command => "tar -zxvf /tmp/s3cmd-${s3cmd::params::commit_sha1_abbreviation}.tar.gz -C /tmp"
   }
 
-  exec { "install-s3cmd":
-    path       => "/bin",
-    command    => "python setup.py install",
-    require    => [Exec["untar-s3cmd"], Package["python-setuptools"]]
-    cwd        => "/tmp/s3tools-s3cmd-${s3cmd::params::commit_sha1_abbreviation}",
+  exec { 'install-s3cmd':
+    cwd     => "/tmp/s3tools-s3cmd-${s3cmd::params::commit_sha1_abbreviation}",
+    command => 'python setup.py install',
+    require => [Exec['untar-s3cmd'], Package['python-setuptools']]
   }
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,5 +1,5 @@
 class s3cmd::params {
   # 7 digits is the Git default for a short SHA
   $commit_sha1_abbreviation = '6cdff22'
-  $cache_dir   = '/tmp/vagrant-cache'
+  $cache_dir                = '/tmp/vagrant-cache'
 }
